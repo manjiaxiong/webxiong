216f3c8c827ec84a11d936f4db00ef85bf9d7ca3
xiong <1957921021@qq.com>
2019-06-03 20:57:12 +0800

    as

---
+++ 小米网站/js/date.js
@@ -0,0 +1,14 @@
+var aNavDate=[
+			[
+			{},{}],
+]
+<li>
+						<a href="#">
+						<div class="tag">新品</div>
+						<div class="img-box">
+							<img src="../images/mi9-320.png" alt="">
+							</div>
+						<p class="header-nav-content-name">小米九</p>
+						<p class="header-nav-content-price">2999元</p>
+						</a>
+					</li>
