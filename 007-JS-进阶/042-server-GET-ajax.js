18f6a5d96f226d56d934ebde663e3a2b0d65aa8f
xiongjiaman <1957921021@qq.co>
2019-06-19 15:31:48 +0800

    am 15.31

--- 007-JS-进阶/042-server-GET-ajax.js
+++ 007-JS-进阶/042-server-GET-ajax.js
@@ -5,13 +5,13 @@ var port=3000;
 var server=http.createServer(function(req,res){
  	console.log(req.url);
  	var filePath='./'+req.url;
- 	fs.readFile(filePath,function(err,date){
+ 	fs.readFile(filePath,function(err,data){
  		if(err){
  			res.statusCode=404;
  			res.end('not found');
  		}else{
  			res.statusCode=200;
- 			res.end(date);
+ 			res.end(data);
  		}
  	})
  })
