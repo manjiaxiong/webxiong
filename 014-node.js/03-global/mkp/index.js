52268c13e1e180c934049dab90af2d263093329c
xiongjiaman <1957921021@qq.co>
2019-07-24 15:45:15 +0800

    as

---
+++ 014-node.js/03-global/mkp/index.js
@@ -0,0 +1,22 @@
+//指定项目名称，创建前端项目的目录结构
+//1获取名称
+const fs=require('fs');
+
+// const pathName=process.argv[2];
+// // console.log(pathName)
+// //2根据名称生成文件
+// fs.mkdirSync(pathName);
+// fs.mkdirSync(pathName+'/css');
+// fs.mkdirSync(pathName+'/js');
+// fs.mkdirSync(pathName+'/images');
+//封装函数
+function mkp(){
+	const pathName=process.argv[2];
+	// console.log(pathName)
+	//2根据名称生成文件
+	fs.mkdirSync(pathName);
+	fs.mkdirSync(pathName+'/css');
+	fs.mkdirSync(pathName+'/js');
+	fs.mkdirSync(pathName+'/images');
+}
+module.exports=mkp;
